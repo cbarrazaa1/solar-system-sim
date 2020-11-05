95dbffcef188debdb8da5d4f3ca5cc80d43994c0
Cesar Barraza <cbarrazaa1@gmail.com>
2020-11-05 17:46:31 -0600

    added skybox

---
+++ src/Skybox.ts
@@ -0,0 +1,16 @@
+import {TextureLoader, BoxGeometry, Mesh, MeshBasicMaterial, BackSide} from 'three';
+
+const loader = new TextureLoader();
+const imgs = [
+  loader.load('/textures/skybox/milkyway_view.jpg'),
+  loader.load('/textures/skybox/milkyway_view.jpg'),
+  loader.load('/textures/skybox/milkyway_view.jpg'),
+  loader.load('/textures/skybox/milkyway_view.jpg'),
+  loader.load('/textures/skybox/milkyway_view.jpg'),
+  loader.load('/textures/skybox/milkyway_view.jpg'),
+];
+const sides = imgs.map(img => new MeshBasicMaterial({map: img, side: BackSide}));
+const skyboxBuffer = new BoxGeometry(10000, 10000, 10000);
+const skybox = new Mesh(skyboxBuffer, sides);
+
+export default skybox;
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 import {FirstPersonControls} from 'three/examples/jsm/controls/FirstPersonControls';
+import Skybox from './Skybox';
 
 // create clock
 const clock = new THREE.Clock();
@@ -7,10 +8,10 @@ const clock = new THREE.Clock();
 // create scene
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
-  75,
+  55,
   window.innerWidth / window.innerHeight,
-  0.1,
-  1000,
+  45,
+  30000,
 );
 
 // create renderer
@@ -26,16 +27,20 @@ const sphereMaterial = new THREE.MeshBasicMaterial({color: 0xffffff});
 const sphere = new THREE.Mesh(sphereBuffer, sphereMaterial);
 scene.add(sphere);
 
+// setup skybox
+scene.add(Skybox);
+
 // setup camera
 camera.position.x = 5;
 camera.position.y = 5;
 camera.position.z = 5;
+camera.position.set(1200, -250, 2000);
 camera.lookAt(scene.position);
 
 // setup camera controller
 const cameraController = new FirstPersonControls(camera, renderer.domElement);
 cameraController.lookSpeed = 0.25;
-cameraController.movementSpeed = 8;
+cameraController.movementSpeed = 200;
 
 function animate(): void {
   const delta = clock.getDelta();
