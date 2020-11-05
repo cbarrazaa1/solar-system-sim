64841290d5e8eb25d6d8c5dc67cf8c3d350f1f5d
Cesar Barraza <cbarrazaa1@gmail.com>
2020-11-05 17:16:16 -0600

    added first person camera

--- src/index.ts
+++ src/index.ts
@@ -1,68 +1,51 @@
-// add styles
-import './style.css';
-// three.js
-import * as THREE from 'three';
+import * as THREE from "three";
+import {FirstPersonControls} from 'three/examples/jsm/controls/FirstPersonControls';
 
-// create the scene
+// create clock
+const clock = new THREE.Clock();
+
+// create scene
 const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000,
+);
 
-// create the camera
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
+// create renderer
 const renderer = new THREE.WebGLRenderer();
-
-// set size
 renderer.setSize(window.innerWidth, window.innerHeight);
 
-// add canvas to dom
+// add renderer into DOM
 document.body.appendChild(renderer.domElement);
 
-// add axis to the scene
-const axis = new THREE.AxesHelper(10);
+// create sphere
+const sphereBuffer = new THREE.SphereBufferGeometry(0.5, 16, 8);
+const sphereMaterial = new THREE.MeshBasicMaterial({color: 0xffffff});
+const sphere = new THREE.Mesh(sphereBuffer, sphereMaterial);
+scene.add(sphere);
 
-scene.add(axis);
-
-// add lights
-const light = new THREE.DirectionalLight(0xffffff, 1.0);
-
-light.position.set(100, 100, 100);
-
-scene.add(light);
-
-const light2 = new THREE.DirectionalLight(0xffffff, 1.0);
-
-light2.position.set(-100, 100, -100);
-
-scene.add(light2);
-
-const material = new THREE.MeshBasicMaterial({
-  color: 0xaaaaaa,
-  wireframe: true,
-});
-
-// create a box and add it to the scene
-const box = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), material);
-
-scene.add(box);
-
-box.position.x = 0.5;
-box.rotation.y = 0.5;
-
+// setup camera
 camera.position.x = 5;
 camera.position.y = 5;
 camera.position.z = 5;
-
 camera.lookAt(scene.position);
 
+// setup camera controller
+const cameraController = new FirstPersonControls(camera, renderer.domElement);
+cameraController.lookSpeed = 0.25;
+cameraController.movementSpeed = 8;
+
 function animate(): void {
+  const delta = clock.getDelta();
   requestAnimationFrame(animate);
   render();
+  cameraController.update(delta);
 }
 
 function render(): void {
-  const timer = 0.002 * Date.now();
-  box.position.y = 0.5 + 0.5 * Math.sin(timer);
-  box.rotation.x += 0.01;
+  sphere.rotation.x += 0.01;
   renderer.render(scene, camera);
 }
 
